58d6749dfe8314ca0a9f696cee7e8d93dcedbd59
Banji Ajayi <banji.ajayi@holidayextras.com>
2023-10-18 16:24:42 +0100

    home page done in acordance to tickiet

---
+++ frontend/src/app/pages/homepage/Homepage.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState} from "react";
+
+import styles from './homepage.css'
+
+const Home = ({ navigate }) => {
+  
+    const [token, setToken] = useState(window.localStorage.getItem("token"));
+    const [posts, setPosts] = useState([]);
+    const [user, setUser] = useState([]);
+  
+    useEffect(() => {
+      if(token) {
+        fetch("/users/home", {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+          .then(response => response.json())
+          .then(async data => {
+            window.localStorage.setItem("token", data.token)
+            setToken(window.localStorage.getItem("token"))
+            setUser(data.user);
+          })
+      }
+    }, [token, setToken, setUser])
+    
+    const logout = () => {
+      window.localStorage.removeItem("token")
+      navigate('/login')
+    }
+
+    if(token) {
+        return(
+          <>
+            <div id='homepage' className={styles.homepage}>
+              <h1>Homepage</h1>
+              <h2>listings</h2>
+              <div id='homepage-content' className={styles.content}>
+                {/* this should contain the listing feed componaet             */}
+              </div>
+            </div>
+          </>
+        )
+      } else {
+        navigate('/login')
+      }
+    }
+    
+    export default Home;
