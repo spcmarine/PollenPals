5b1702f7b1f38450582bb377e036d350af8f4ae5
Banji Ajayi <banji.ajayi@holidayextras.com>
2023-10-18 14:25:48 +0100

    login page and sign up page working

--- frontend/src/app/app.jsx
+++ frontend/src/app/app.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './app.css';
 import LoginForm from './pages/login/LoginForm';
+import SignupForm from './pages/signup/Signup';
 import {
     useNavigate,
     Routes,
@@ -11,6 +12,7 @@ const App = () => {
     return (
         <Routes>
             <Route path='/login'  element={<LoginForm  navigate={ useNavigate() } />}/>
+            <Route path='/signup' element={<SignupForm navigate={ useNavigate() } /> }/>
         </Routes>
     )
 }
--- frontend/src/app/pages/signup/signup.jsx
+++ frontend/src/app/pages/signup/signup.jsx
@@ -1,2 +1,65 @@
 import React, {useState} from "react";
-import './LoginForm.css';
+import './signup.css';
+
+const SignUpForm = ({ navigate }) => {
+    // State variables to manage the email and password input values
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+
+const handleSubmit = async (event) =>{
+    event.preventDefault();
+
+    // Send a POST request to the '/users' endpoint with email and password data and name 1st and 2nd
+    fetch('/users', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+          email: email, 
+          password: password, 
+          firstName: firstName, 
+          lastName: lastName,
+        })
+      })
+        .then(response => {
+          // Check the response status code
+          if (response.status === 201) {
+            // If the status code is 201 (Created), navigate to the login page
+            navigate('/login');
+          } else {
+            // If the status code is not 201, navigate back to the signup page
+            navigate('/signup');
+          }
+        })
+    }
+
+
+    return(
+        <div>
+            <form>
+                <p>Email: </p>
+                <input placeholder='Email' id='email' type='text' value={ email }></input>
+                <p>Password: </p>
+                <input placeholder="Password"></input>
+                <p>ConfriemPassword: </p>
+                <input placeholder="Comfirme Password"></input>
+                <p>FirstName:</p>
+                <input placeholder="fristname"></input>
+                <p>SecondName</p>
+                <input placeholder="second name" type="text" />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm;
+
+
+
+
+
