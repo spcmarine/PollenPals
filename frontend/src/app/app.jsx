77bbbbf256fd5a9aec57db933e4adeb136192e4f
Alice Birkett <alice.birkett@outlook.com>
2023-10-23 16:44:34 +0100

    updated styling

--- frontend/src/app/app.jsx
+++ frontend/src/app/app.jsx
@@ -4,7 +4,7 @@ import LoginForm from './pages/login/LoginForm';
 import SignupForm from './pages/signup/signup';
 import Listings from './pages/Listings/listings.jsx'
 import Homepage from './pages/Homepage/Homepage'
-import CreateListing from './pages/createListing/CreateListing.jsx'
+import CreateListing from './pages/CreateListing/CreateListing.jsx'
 
 import {
     useNavigate,
@@ -19,7 +19,7 @@ const App = () => {
             <Route path='/login'  element={<LoginForm  navigate={ useNavigate() } />}/>
             <Route path='/signup' element={<SignupForm navigate={ useNavigate() } /> }/>
             <Route path='/listings' element={<Listings navigate={ useNavigate() } /> }/>
-            <Route path='/createListing' element={<CreateListing navigate={ useNavigate() } /> }/>
+            <Route path='/create-listing' element={<CreateListing navigate={ useNavigate() } /> }/>
         </Routes>
     )
 }
