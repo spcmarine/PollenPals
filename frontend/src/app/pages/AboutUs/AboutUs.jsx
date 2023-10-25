88856affcab53a2cbf569c261220fd6d45936f99
Dan Gibson <spcmarine@btinternet.com>
2023-10-25 11:56:41 +0100

    basic CCS added

--- frontend/src/app/pages/AboutUs/AboutUs.jsx
+++ frontend/src/app/pages/AboutUs/AboutUs.jsx
@@ -6,16 +6,18 @@ const AboutUs = ({ navigate }) => {
 
     return(
         <div className={styles.aboutUs}>
+            <div className={styles.pageWithNav}>
             <SignedOutNavbar className={styles.navbar} navigate={navigate}/>
             <div className={styles.aboutUsContent}>
                 <h1 className={styles.aboutUsTitle}>About Us</h1>
                 <div className={styles.aboutUsText}>
-                    <p>PlantSwap is a platform for plant lovers to connect with other plant lovers and swap plants. 
+                    <p>PollenPals is a platform for plant lovers to connect with other plant lovers and swap plants. 
                         We are a community of people who love plants and want to share our love of plants with others. 
                         We believe that plants are a great way to connect with others and we want to help you find the perfect plant for you. 
                         We hope you enjoy using our platform as much as we enjoyed creating it!
                     </p>
                 </div>
+            </div>
             </div>
         </div>  
     )
